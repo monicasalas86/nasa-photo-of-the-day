fcccfbde385c52d86635f6af01740c965d761edc
monicasalas86 <monicsalas167@gmail.com>
2021-09-08 16:42:35 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from '../src/components/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,2 @@
-`https://api.nasa.gov/planetary/apod?api_key=BZR4pKnRyIJnc6h8hTbL9z45MturqMMxyGLA7ovR`
+export const BASE_URL = `https://api.nasa.gov/planetary/apod?api_key=`
+export const API_KEY = `BZR4pKnRyIJnc6h8hTbL9z45MturqMMxyGLA7ovR`
