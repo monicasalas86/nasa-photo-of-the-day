ee9774c6bdd6187b1deb042552c17174fc41ad22
monicasalas86 <monicsalas167@gmail.com>
2021-09-08 21:07:45 -0500

    finished photo section

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -6,28 +6,47 @@ import { BASE_URL, API_KEY } from '../constants/index'
 const StyledCard = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    align-items: center;
     width: 80vw;
-    border: 2px solid black;
 
     .card-container {
         display: flex;
         flex-direction: column;
-        justify-content: center;
-        border: 2px solid green;
+        align-items: center;
+        /* border: 2px solid pink; */
+        border-radius: 10px;
+        width: 60%;
+        height: 100%;
+        margin-top: 10px;
+        background-color: white;
     }
 
     .card-title {
-        border: 2px solid red;
+        /* border: 2px solid red; */
         display: flex;
         flex-direction: row;
         justify-content: space-between;
+        color: slategray;
+        width: 80%;
+    }
+    .card-body {
+        /* border: 3px solid green; */
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    .description {
+        /* border: 2px solid orange; */
+        width: 95%;
+        font-size: 12px;
+        color: darkslategray
     }
 `
 const NasaImg = styled.img`
-    border: 2px solid purple;
-    height: 600px;
-    width: 750px;
+    height: 450px;
+    width: 80%;
+    border-radius: 10px;
 `
 
 export default function PhotoCard({date, title, description}) {
@@ -47,8 +66,8 @@ export default function PhotoCard({date, title, description}) {
         <StyledCard>
             <div className='card-container'>
                 <div className='card-title'>
-                    <p className='card-date'>{date}</p>
                     <p className='image-title'>{title}</p>
+                    <p className='card-date'>{date}</p>
                 </div>
                 <div className='card-body'>
                     <NasaImg className='card-img' src={dailyPic} alt='NASA pic of the day'/>
